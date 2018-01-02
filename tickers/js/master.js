0e2b72f040de075941235d29612e836b1d338761
Hassan AlMandil <mandil@Hassans-MacBook-Pro.local>
2018-01-02 03:20:33 -0800

    Access-Control-Allow-Origin set to https://mandil.github.io

--- tickers/js/master.js
+++ tickers/js/master.js
@@ -3,7 +3,7 @@ $(()=>{
   $('.test').append(`<h1>test</h1>`);
   $.ajax({
     url: 'https://bittrex.com/api/v1.1/public/getmarkets',
-    headers: {  'Access-Control-Allow-Origin': '*' },
+    headers: {  'Access-Control-Allow-Origin': 'https://mandil.github.io' },
     type: 'GET',
      success: function(res) { alert(`Success ${res}`); },
      error: function(res) { alert(`Error ${res}`); }
