f1fef5ebd01fb1a92c06d9e802adfeb1ceaaaddd
“Hassan <hassan.almandeel@gmail.com>
2018-01-10 14:44:59 -0800

    api check

--- tickers/js/master.js
+++ tickers/js/master.js
@@ -1,8 +1,8 @@
 
   import * as bittrex from 'node-bittrex-api';
   bittrex.options({
-    'apikey' : API_KEY,
-    'apisecret' : API_SECRET,
+    'apikey' : '868145ed286844318964abf6c6959ab3',
+    'apisecret' : 'aec47260f81a49dfaaa1609449b69ff0',
   });
   bittrex.getmarketsummaries( function( data, err ) {
     if (err) {
