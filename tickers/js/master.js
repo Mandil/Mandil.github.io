30a0310d664c14199a0949fa7aa33f61240189c8
“Hassan <hassan.almandeel@gmail.com>
2018-01-10 14:10:51 -0800

    update api

--- tickers/js/master.js
+++ tickers/js/master.js
@@ -3,10 +3,8 @@ $(()=>{
   $('.test').append(`<h1>test</h1>`);
   $.ajax({
     url: 'https://bittrex.com/api/v1.1/public/getmarkets',
-    headers: {  'Access-Control-Allow-Origin': 'https://mandil.github.io' },
-    type: 'GET',
-     success: function(res) { alert(`Success ${res}`); },
-     error: function(res) { alert(`Error ${res}`); }
+    success: function(res) { console.log(`Success ${res}`); },
+    error: function(res) { console.log(`Error ${res}`); }
   })
 
 
