53931512ce278e8485849497c89eb8cebed16974
“Hassan <hassan.almandeel@gmail.com>
2018-01-10 14:16:01 -0800

    Access-Control-Allow-Origin error

--- tickers/js/master.js
+++ tickers/js/master.js
@@ -3,6 +3,7 @@ $(()=>{
   $('.test').append(`<h1>test</h1>`);
   $.ajax({
     url: 'https://bittrex.com/api/v1.1/public/getmarkets',
+    dataType: 'jsonp',
     success: function(res) { console.log(`Success ${res}`); },
     error: function(res) { console.log(`Error ${res}`); }
   })
