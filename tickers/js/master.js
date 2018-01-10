7be58e5013fc41f5245ba6de765b57aea50e9f2f
“Hassan <hassan.almandeel@gmail.com>
2018-01-10 14:20:02 -0800

    install node.bittrex.api

--- tickers/js/master.js
+++ tickers/js/master.js
@@ -1,12 +1,28 @@
 $(()=>{
 
-  $('.test').append(`<h1>test</h1>`);
-  $.ajax({
-    url: 'https://bittrex.com/api/v1.1/public/getmarkets',
-    dataType: 'jsonp',
-    success: function(res) { console.log(`Success ${res}`); },
-    error: function(res) { console.log(`Error ${res}`); }
-  })
+  var bittrex = require('node-bittrex-api');
+  bittrex.options({
+    'apikey' : API_KEY,
+    'apisecret' : API_SECRET,
+  });
+  bittrex.getmarketsummaries( function( data, err ) {
+    if (err) {
+      return console.error(err);
+    }
+    for( var i in data.result ) {
+      bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
+        console.log( ticker );
+      });
+    }
+  });
+
+  // $('.test').append(`<h1>test</h1>`);
+  // $.ajax({
+  //   url: 'https://bittrex.com/api/v1.1/public/getmarkets',
+  //   dataType: 'jsonp',
+  //   success: function(res) { console.log(`Success ${res}`); },
+  //   error: function(res) { console.log(`Error ${res}`); }
+  // })
 
 
 })
